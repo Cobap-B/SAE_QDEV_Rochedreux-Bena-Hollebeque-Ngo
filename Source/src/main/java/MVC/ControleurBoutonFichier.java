208e834d273902c35803a8c433091f6d75c0485e
Kevin Ngo <kevin.ngo8@etu.univ-lorraine.fr>
2024-12-18 16:36:41 +0100

    ajout squelette ControleurBoutonFichier et VueBoutonFichier

---
+++ Source/src/main/java/MVC/ControleurBoutonFichier.java
@@ -0,0 +1,11 @@
+package MVC;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class ControleurBoutonFichier implements EventHandler<ActionEvent> {
+    @Override
+    public void handle(ActionEvent actionEvent) {
+
+    }
+}
