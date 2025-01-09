a0e31bfdb361ec36a42a844ecb93047020815833
Cobap-B <Chugo.rochedreux@gmail.com>
2025-01-09 10:19:29 +0100

    Modif test

--- Source/src/main/java/Classes/FichierComposite.java
+++ Source/src/main/java/Classes/FichierComposite.java
@@ -5,8 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class FichierComposite extends File {
-    public List<Dossier> files =new ArrayList<>();
-    public List<Classe> file2s =new ArrayList<>();
     public FichierComposite(String pathname) {
         super(pathname);
     }
